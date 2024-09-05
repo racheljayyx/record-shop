4a6405b74f73f806de5789595c7d9bb3fa554fa1
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-05 09:53:57 +0100

    first commit

---
+++ src/main/java/com/northcoders/RecordShop/RecordShopApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.RecordShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/RecordShop/RecordShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.RecordShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
