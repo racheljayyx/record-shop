716027c0fa9292165800a90713cdbcc35b81cd30
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-10 09:32:47 +0100

    feat: implement logic to pass GET  all records unit test for record shop controller"

--- src/main/java/com/northcoders/RecordShop/controller/RecordShopController.java
+++ src/main/java/com/northcoders/RecordShop/controller/RecordShopController.java
@@ -1,10 +1,16 @@
 package com.northcoders.RecordShop.controller;
 
 
+import com.northcoders.RecordShop.model.Record;
 import com.northcoders.RecordShop.service.RecordShopService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/recordshop")
@@ -12,4 +18,10 @@ public class RecordShopController {
 
     @Autowired
     RecordShopService recordShopService;
+
+    @GetMapping
+    public ResponseEntity<List<Record>> getAllRecords() {
+        List<Record> records = recordShopService.getAllRecords();
+        return new ResponseEntity<>(records, HttpStatus.OK);
+    }
 }
