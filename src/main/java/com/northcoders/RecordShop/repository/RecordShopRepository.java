859e64310e9590f833019400d74b7879ce0815cc
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-10 08:31:45 +0100

    feat: update RecordShopRepository to extend from the CrudRepository class

--- src/main/java/com/northcoders/RecordShop/repository/RecordShopRepository.java
+++ src/main/java/com/northcoders/RecordShop/repository/RecordShopRepository.java
@@ -1,7 +1,9 @@
 package com.northcoders.RecordShop.repository;
 
+import com.northcoders.RecordShop.model.Record;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecordShopRepository{
+public interface RecordShopRepository extends CrudRepository<Record, Long> {
 }
