71a04e56aee219ee52b7ba830d11f22a6cb375c6
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-10 11:08:51 +0100

    feat: implement logic to pass unit test to GET alnum by id for record shop service

--- src/main/java/com/northcoders/RecordShop/service/RecordShopService.java
+++ src/main/java/com/northcoders/RecordShop/service/RecordShopService.java
@@ -3,8 +3,10 @@ package com.northcoders.RecordShop.service;
 import com.northcoders.RecordShop.model.Record;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RecordShopService {
     List<Record> getAllRecords();
+    Optional<Record> getRecordById(Long id);
 
 }
