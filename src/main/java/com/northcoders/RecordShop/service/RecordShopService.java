289c15cf9e74125156b7852defc0f1cc2e6f6cf7
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-10 09:06:51 +0100

    feat: implement logic to pass GET all album unit test"

--- src/main/java/com/northcoders/RecordShop/service/RecordShopService.java
+++ src/main/java/com/northcoders/RecordShop/service/RecordShopService.java
@@ -1,4 +1,10 @@
 package com.northcoders.RecordShop.service;
 
+import com.northcoders.RecordShop.model.Record;
+
+import java.util.List;
+
 public interface RecordShopService {
+    List<Record> getAllRecords();
+
 }
