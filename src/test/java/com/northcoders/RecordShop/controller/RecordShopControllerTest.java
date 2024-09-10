6d40007b369d21a4751791f579a2722b04dd7c76
Rachel Jaisey <jaiseyrachel@gmail.com>
2024-09-10 09:50:14 +0100

    feat(REC-2): add feature to GET all albums

---
+++ src/test/java/com/northcoders/RecordShop/controller/RecordShopControllerTest.java
@@ -0,0 +1,67 @@
+package com.northcoders.RecordShop.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.northcoders.RecordShop.model.Format;
+import com.northcoders.RecordShop.model.Genre;
+import com.northcoders.RecordShop.model.Record;
+import com.northcoders.RecordShop.service.RecordShopServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+public class RecordShopControllerTest {
+
+    @Mock
+    private RecordShopServiceImpl mockRecordShopServiceImpl;
+
+    @InjectMocks
+    private RecordShopController recordShopController;
+
+    @Autowired
+    private MockMvc mockMvcController;
+
+    private ObjectMapper mapper;
+
+    @BeforeEach
+    public void setup() {
+        mockMvcController = MockMvcBuilders.standaloneSetup(recordShopController).build();
+        mapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testGetAllRecordsReturnsListOfRecords() throws Exception {
+
+        List<com.northcoders.RecordShop.model.Record> records = new ArrayList<>();
+        records.add(new com.northcoders.RecordShop.model.Record(1L, "The Great Commission", "Dunsin Oyekan", Genre.GOSPEL, Format.DIGITAL, 5, 100));
+        records.add(new com.northcoders.RecordShop.model.Record(2L, "The 50 Greatest Pieces of Classical Music", "London Philharmonic Orchestra", Genre.CLASSICAL, Format.CD, 1, 87));
+        records.add(new Record(3L, "Won't Be Blue Anymore", "Dan Seals", Genre.COUNTRY, Format.VINYL, 19, 69));
+
+        when(mockRecordShopServiceImpl.getAllRecords()).thenReturn(records);
+
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/api/v1/recordshop"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].id").value(1))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].name").value("The Great Commission"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].id").value(2))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].artist").value("London Philharmonic Orchestra"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].id").value(3))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2].quantity").value(19));
+    }
+
+}
